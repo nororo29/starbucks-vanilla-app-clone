e8c2b4048c6f0cd49d8b6b5f5d578c8848563ec7
nororo <wlgus4401@gmail.com>
2022-08-24 01:08:03 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,104 @@ searchInputEl.addEventListener('blur', function () {
 	searchEl.classList.remove('focused');
 	searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+	'scroll',
+	_.throttle(function () {
+		console.log(window.scrollY);
+
+		if (window.scrollY > 500) {
+			// 배지 숨기기
+			// badgeEl.style.display = 'none';
+			// gsap.to(요소, 지속시간, 옵션)
+			gsap.to(badgeEl, 0.6, {
+				opacity: 0,
+				display: 'none',
+			});
+		} else {
+			// 배지 보이기
+			// badgeEl.style.display = 'block';
+			// gsap.to(요소, 지속시간, 옵션)
+			gsap.to(badgeEl, 0.6, {
+				opacity: 1,
+				display: 'block',
+			});
+		}
+	}, 300)
+);
+// _.throttle(함수, 시간) : 몇초에 한번씩 함수 실행되도록
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function (fadeEl, index) {
+	gsap.to(fadeEl, 1, {
+		delay: (index + 1) * 0.7,
+		opacity: 1,
+	});
+});
+
+// SWIPER
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+	direction: 'vertical',
+	autoplay: true, // 기본값 3000 3초
+	loop: true,
+});
+
+new Swiper('.promotion .swiper', {
+	// direction: 'horizontal',
+	slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+	spaceBetween: 10, // 슬라이드 사이 여백
+	centeredSlides: true, // 1번 슬라이드가 가운데
+	loop: true,
+	// autoplay: {
+	// 	delay: 500,
+	// },
+	pagination: {
+		el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+		clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+	},
+	navigation: {
+		prevEl: '.promotion .swiper-prev',
+		nextEl: '.promotion .swiper-next',
+	},
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+	isHidePromotion = !isHidePromotion;
+	if (isHidePromotion) {
+		// 숨김처리
+		promotionEl.classList.add('hide');
+	} else {
+		// 보임처리
+		promotionEl.classList.remove('hide');
+	}
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+	// `.toFixed()`를 통해 반환된 문자 데이터를,
+	// `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+	return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+	// gsap.to(요소, 시간, 옵션);
+	gsap.to(selector, random(1.5, 2.5), {
+		y: size,
+		repeat: -1,
+		yoyo: true, // 애니메이션이 다시 돌아오는 속성
+		ease: Power1.easeInOut,
+		delay: random(0, delay),
+	});
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
